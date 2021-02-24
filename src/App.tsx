f99f00dfb0a8f115603a83b19f389972451feae7
Dunridge <Polinka7max@gmail.com>
2021-02-24 08:27:27 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <p className="app__title">Hi</p>
+      <p className="app__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          A blanditiis cum, deserunt doloremque doloribus, eius eligendi eos eum excepturi facilis
+          fugit iste, laborum nemo possimus suscipit? Aperiam fuga ratione ut.</p>
+    </div>
+  );
+}
+
+export default App;
